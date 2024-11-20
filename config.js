b928cdd22b8cf53d84cb62ece954c911935042f7
Ginas559 <140213980+Ginas559@users.noreply.github.com>
2024-11-20 16:12:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Cô giáo thân yêu';
+const nameGirl = 'Đặng Ánh Mai';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-11';
-const titleCard = 'Tặng cô giáo của con';
-const contentCard = 'Nhân ngày Nhà giáo Việt Nam 20/11, con kính chúc cô thật nhiều sức khỏe, luôn vui vẻ và hạnh phúc. Cảm ơn cô đã luôn tận tâm dạy dỗ chúng con.';
+const titleCard = 'Tặng cô giáo';
+const contentCard = 'Nhân ngày Nhà giáo Việt Nam 20/11, kính chúc cô thật nhiều sức khỏe, luôn vui vẻ và hạnh phúc.';
 
 const giftImage = 'teacher.png'; // Hình ảnh liên quan đến cô giáo (có thể thay thế bằng hình ảnh phù hợp)
 const base64 = '';
